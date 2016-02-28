1653ddd42d6742c0ac7b56e37ae31398ff6ad3f6
Tim Coker <cokert@gmail.com>
2016-02-28 01:05:27 -0500

    Initial import
    implementation of assignment instructions

---
+++ plot1.R
@@ -0,0 +1,30 @@
+library(readr)
+library(lubridate)
+
+#read file
+dat<-read_delim("household_power_consumption.txt", delim =";", col_types = "ccnnnnnnn", na="?" )
+
+#fix column names
+cn = names(dat)
+cn = tolower(cn)        #lower case
+cn = gsub("_", "", cn)  #remove underscore
+cn[1]="mydate"          #rename "date" to "mydate" because of conflict with base::date
+names(dat) = cn
+
+#dates in this file are in d/m/yyyy format
+dat<-dat %>% filter(mydate=="1/2/2007" | mydate=="2/2/2007")
+
+#create datetime column using lubridate function
+dat$datetime<-dmy_hms(paste(dat$mydate,dat$time))
+
+#open device/file
+png(file="plot1.png")
+
+#set transparent background
+par(bg=NA)
+
+#generate plot
+hist(dat$globalactivepower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+#close device/file
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,37 @@
+library(readr)
+library(lubridate)
+
+#read file
+dat<-read_delim("household_power_consumption.txt", delim =";", col_types = "ccnnnnnnn", na="?" )
+
+#fix column names
+cn = names(dat)
+cn = tolower(cn)        #lower case
+cn = gsub("_", "", cn)  #remove underscore
+cn[1]="mydate"          #rename "date" to "mydate" because of conflict with base::date
+names(dat) = cn
+
+#dates in this file are in d/m/yyyy format
+dat<-dat %>% filter(mydate=="1/2/2007" | mydate=="2/2/2007")
+
+#create datetime column using lubridate function
+dat$datetime<-dmy_hms(paste(dat$mydate,dat$time))
+
+png(file="plot3.png")
+
+#set transparent background
+par(bg=NA)
+
+#generate empty plot
+plot(dat$datetime, dat$submetering1, type="n", ylab="Energy sub metering", xlab="")
+
+#add the three series
+points(dat$datetime, dat$submetering1, type="l", col="black")
+points(dat$datetime, dat$submetering2, type="l", col="red")
+points(dat$datetime, dat$submetering3, type="l", col="blue")
+
+#add a legend
+legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1)
+
+#close device/file
+dev.off()
